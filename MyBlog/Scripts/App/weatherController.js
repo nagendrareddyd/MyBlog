2ec77285ba1691b05db82af637a5d302ebe9c53f
Nagendra <nagendrareddy.d@gmail.com>
2015-07-17 22:05:15 +1000

    live weather report working

--- MyBlog/Scripts/App/weatherController.js
+++ MyBlog/Scripts/App/weatherController.js
@@ -1,15 +1,23 @@
 ﻿(function () {
-    
-    var weatherController = function ($scope, weatherService, appConstants) {
 
-        var liveWeatherResponse = function (response) {
-            $scope.jsonData = response;
+    var weatherController = function ($scope, weatherService, appConstants) {
+        var weatherResponseSuccess = function (response) {
+            $scope.weatherData = {
+                weatherIcon: 'http://openweathermap.org/img/w/' + response.data.weather[0].icon + '.png',
+                name: response.data.name + ',' + response.data.sys.country,
+                icon: response.data.weather[0].icon,
+                temperature: response.data.main.temp,
+                wind: response.data.wind.speed,
+                clouds: response.data.clouds.all
+            }
         }
         var onError = function (error) {
             $scope.error = appConstants.weatherNotFound;
         }
 
-        weatherService.getliveWeatherByCity('6619279').then(liveWeatherResponse, onError);
+        $scope.getWeatherData = function (cityId) {
+            weatherService.getliveWeatherByCity(cityId).then(weatherResponseSuccess, onError);
+        }
     }
     var app = angular.module('mainApp');
     app.controller('weatherController', weatherController);
--- MyBlog/Scripts/App/weatherService.js
+++ MyBlog/Scripts/App/weatherService.js
@@ -1,15 +1,16 @@
 ﻿(function () {
 
-    var liveWeatherService = function ($http) {
+    var weatherLiveService = function ($http) {
         return {
             getliveWeatherByCity: function (cityId) {
-                var apiUrl = 'http://api.openweathermap.org/data/2.5/weather?callback=JSON_CALLBACK&id=' + cityId;
-                return $http.jsonp(apiUrl);
+                var urlApi = 'http://api.openweathermap.org/data/2.5/weather?callback=JSON_CALLBACK&units=metric&id=' + cityId;
+                return $http.jsonp(urlApi);
+                // cross domain issue - https://jvaneyck.wordpress.com/2014/01/07/cross-domain-requests-in-javascript/
             }
         }
     };
 
     var app = angular.module('mainApp');
-    app.factory('weatherService', liveWeatherService);
+    app.factory('weatherService', weatherLiveService);
 
 }());
